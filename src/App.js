cd9d55221fc370f87a573ca55a4a8783956e9f1a
Jiyun <12jiyun@gmail.com>
2024-10-08 02:11:47 +0900

    add weather api & using geolocation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const API_KEY = process.env.REACT_APP_WEATHER_KEY;
+    console.log(process.env.REACT_APP_WEATHER_KEY);
+    const [weather, setWeather] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const getWeatherByCurrentLocation = async (lat, lon) => {
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+        setLoading(true);
+        let response = await fetch(url);
+        let data = await response.json();
+        setWeather(data);
+        setLoading(false);
+        console.log("현재 날씨는?", data);
+    };
+
+    const getCurrentLocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+
+            console.log("현재 위치는?", lat, lon);
+
+            getWeatherByCurrentLocation(lat, lon);
+        });
+    };
+
+    return (
+        <div className="App">
+            <h1>Weather App</h1>
+            <button onClick={getCurrentLocation}>{loading ? "Loading..." : "Get Current Location Weather"}</button>
+
+            {weather && (
+                <div>
+                    <h2>{weather.name}</h2>
+                    <p>{weather.weather[0].description}</p>
+                    <p>{Math.round(weather.main.temp)}°C</p>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
