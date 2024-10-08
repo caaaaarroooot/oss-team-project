a5c81b123713e467e68c9bcbf3ebce82cd2c67be
Jiyun <12jiyun@gmail.com>
2024-10-08 11:04:22 +0900

    add pages

---
+++ src/component/Weather.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function Weather() {
+    const API_KEY = process.env.REACT_APP_WEATHER_KEY;
+    console.log(process.env.REACT_APP_WEATHER_KEY);
+    const [weather, setWeather] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const getWeatherByCurrentLocation = async (lat, lon) => {
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+        setLoading(true);
+        let response = await fetch(url);
+        let data = await response.json();
+        setWeather(data);
+        setLoading(false);
+        console.log("현재 날씨는?", data);
+    };
+
+    const getCurrentLocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+
+            console.log("현재 위치는?", lat, lon);
+
+            getWeatherByCurrentLocation(lat, lon);
+        });
+    };
+
+    return (
+        <div>
+            <h1>Weather App</h1>
+            <button onClick={getCurrentLocation}>{loading ? "Loading..." : "Get Current Location Weather"}</button>
+
+            {weather && (
+                <div>
+                    <h2>{weather.name}</h2>
+                    <p>{weather.weather[0].description}</p>
+                    <p>{Math.round(weather.main.temp)}°C</p>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Weather;
