ed24ba75f683d08098838479dd8052db2a611544
Jiyun <12jiyun@gmail.com>
2024-10-19 03:14:06 +0900

    list route add

---
+++ src/pages/ListPage.js
@@ -0,0 +1,9 @@
+function ListPage() {
+    return (
+        <>
+            <div>hello</div>
+        </>
+    );
+}
+
+export default ListPage;
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FormPage from "../pages/FormPage";
+import ListPage from "../pages/ListPage";
 
 const AppRouter = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<FormPage />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<ListPage />} />
+                <Route path="/form" element={<FormPage />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default AppRouter;
