6c1e48e34379a49c8acd8dbe0b72d60a5a550ce4
Jiyun <12jiyun@gmail.com>
2024-10-20 04:08:19 +0900

    before add search

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -5,6 +5,7 @@ import { deleteUser, getallUsers } from "../service/api";
 import { Link } from "react-router-dom";
 import Delete from "../assets/image/delete.png";
 import Edit from "../assets/image/edit.png";
+import styled from "styled-components";
 
 const useStyle = makeStyles({
     table: {
@@ -27,8 +28,10 @@ const useStyle = makeStyles({
 
 const ListPage = () => {
     const classes = useStyle();
-
     const [user, setUser] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 5; // 페이지당 표시할 항목 수
+
     useEffect(() => {
         getUsers();
     }, []);
@@ -43,65 +46,322 @@ const ListPage = () => {
         await deleteUser(id);
         getUsers();
     };
+
+    // 의심으로 분류된 사용자 수 계산
+    const suspectedCount = user.filter((data) => data.symptom && data.symptom.length > 0).length;
+    const totalCount = user.length;
+
+    // 현재 페이지에 표시할 사용자 목록 계산
+    const indexOfLastUser = currentPage * itemsPerPage;
+    const indexOfFirstUser = indexOfLastUser - itemsPerPage;
+    const currentUsers = user.slice(indexOfFirstUser, indexOfLastUser);
+
+    const totalPages = Math.ceil(totalCount / itemsPerPage);
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+    const handlePrevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
     return (
-        <Table className={classes.table}>
-            <TableHead>
-                <TableRow>
-                    <TableCell style={{ borderBottom: " 3px solid #A2D9F0" }}>분류</TableCell>
-                    <TableCell style={{ borderBottom: " 3px solid #A2D9F0" }}>이름</TableCell>
-                    <TableCell style={{ borderBottom: " 3px solid #A2D9F0" }}>성별</TableCell>
-                    <TableCell style={{ borderBottom: " 3px solid #A2D9F0" }}>생년월일</TableCell>
-                    <TableCell style={{ borderBottom: " 3px solid #A2D9F0" }}>항공편명</TableCell>
-                    <TableCell style={{ borderBottom: " 3px solid #A2D9F0" }}>출발지</TableCell>
-                    <TableCell style={{ borderBottom: " 3px solid #A2D9F0" }}>연락처</TableCell>
-                    <TableCell style={{ borderBottom: " 3px solid #A2D9F0" }}>옵션</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {user.map((data, index) => (
-                    <TableRow className={classes.trow} key={index}>
-                        <TableCell>{data.symptom && data.symptom.length > 0 ? "의심" : ""}</TableCell>
-                        <TableCell>{data.name}</TableCell>
-                        <TableCell>{data.gender}</TableCell>
-                        <TableCell>{data.birthdate}</TableCell>
-                        <TableCell>{data.flightCode}</TableCell>
-                        <TableCell>{data.departure}</TableCell>
-                        <TableCell>{data.contact}</TableCell>
-                        <TableCell>
-                            <Button
-                                variant="contained"
+        <MotherDiv>
+            <TitleDiv>입국자 목록 관리</TitleDiv>
+            <BodyDiv>
+                <SearchDiv></SearchDiv>
+                <div
+                    style={{
+                        fontSize: "18px",
+                        color: "#B9B9B9",
+                        paddingBottom: "10px",
+                        boxSizing: "border-box",
+                        borderBottom: " 2px solid #DFDFDF",
+                    }}
+                >
+                    총 <span style={{ fontSize: "23px", fontWeight: "900", color: "#070707" }}>{totalCount}</span>명 |
+                    의심 <span style={{ fontWeight: "900", color: "#070707" }}>{suspectedCount}</span>
+                </div>
+                <Table style={{ width: "100%" }}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell
                                 style={{
-                                    margin: "0px 5px",
-                                    backgroundColor: "white",
-                                    border: "1px solid #B5D3F0",
-                                    width: "20px",
+                                    borderBottom: " 3px solid #A2D9F0",
+                                    fontSize: "23px",
+                                    fontWeight: "700",
+                                    color: "#B9B9B9",
                                 }}
-                                component={Link}
-                                to={`/edit/${data.id}`}
                             >
-                                <img src={Edit} alt="edit" width={20} />
-                            </Button>
-                            {/* 4. button label을 cancle에서 delete로 바꿈으로써 버튼의 기능을 직관적으로 명시 */}
-                            <Button
-                                variant="contained"
-                                color="secondary"
+                                순번
+                            </TableCell>
+                            <TableCell
                                 style={{
-                                    padding: "5px",
-                                    margin: "0px 5px",
-                                    backgroundColor: "#FACDCA",
-                                    border: "1px solid #F30B0B",
-                                    width: "20px",
+                                    borderBottom: " 3px solid #A2D9F0",
+                                    fontSize: "23px",
+                                    fontWeight: "700",
+                                    color: "#B9B9B9",
                                 }}
-                                onClick={() => deleteData(data.id)}
                             >
-                                <img src={Delete} alt="delete" width={20} />
-                            </Button>
-                        </TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+                                분류
+                            </TableCell>
+                            <TableCell
+                                style={{
+                                    borderBottom: " 3px solid #A2D9F0",
+                                    fontSize: "23px",
+                                    fontWeight: "700",
+                                    color: "#B9B9B9",
+                                }}
+                            >
+                                이름
+                            </TableCell>
+                            <TableCell
+                                style={{
+                                    borderBottom: " 3px solid #A2D9F0",
+                                    fontSize: "23px",
+                                    fontWeight: "700",
+                                    color: "#B9B9B9",
+                                }}
+                            >
+                                성별
+                            </TableCell>
+                            <TableCell
+                                style={{
+                                    borderBottom: " 3px solid #A2D9F0",
+                                    fontSize: "23px",
+                                    fontWeight: "700",
+                                    color: "#B9B9B9",
+                                }}
+                            >
+                                생년월일
+                            </TableCell>
+                            <TableCell
+                                style={{
+                                    borderBottom: " 3px solid #A2D9F0",
+                                    fontSize: "23px",
+                                    fontWeight: "700",
+                                    color: "#B9B9B9",
+                                }}
+                            >
+                                항공편명
+                            </TableCell>
+                            <TableCell
+                                style={{
+                                    borderBottom: " 3px solid #A2D9F0",
+                                    fontSize: "23px",
+                                    fontWeight: "700",
+                                    color: "#B9B9B9",
+                                }}
+                            >
+                                출발지
+                            </TableCell>
+                            <TableCell
+                                style={{
+                                    borderBottom: " 3px solid #A2D9F0",
+                                    fontSize: "23px",
+                                    fontWeight: "700",
+                                    color: "#B9B9B9",
+                                }}
+                            >
+                                연락처
+                            </TableCell>
+                            <TableCell
+                                style={{
+                                    borderBottom: " 3px solid #A2D9F0",
+                                    fontSize: "23px",
+                                    fontWeight: "700",
+                                    color: "#B9B9B9",
+                                }}
+                            >
+                                옵션
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {currentUsers.map((data, index) => (
+                            <TableRow className={classes.trow} key={index}>
+                                <TableCell
+                                    style={{
+                                        fontSize: "23px",
+                                        fontWeight: "700",
+                                        borderBottom: " 2px solid #DFDFDF",
+                                    }}
+                                >
+                                    {indexOfFirstUser + index + 1}
+                                </TableCell>{" "}
+                                <TableCell
+                                    style={{
+                                        fontSize: "23px",
+                                        fontWeight: "700",
+                                        color: "#FF1F1F",
+                                        borderBottom: " 2px solid #DFDFDF",
+                                    }}
+                                >
+                                    {data.symptom && data.symptom.length > 0 ? "의심" : ""}
+                                </TableCell>
+                                <TableCell
+                                    style={{
+                                        fontSize: "23px",
+                                        fontWeight: "700",
+                                        borderBottom: " 2px solid #DFDFDF",
+                                    }}
+                                >
+                                    {data.name}
+                                </TableCell>
+                                <TableCell
+                                    style={{
+                                        fontSize: "23px",
+                                        fontWeight: "700",
+                                        borderBottom: " 2px solid #DFDFDF",
+                                    }}
+                                >
+                                    {data.gender}
+                                </TableCell>
+                                <TableCell
+                                    style={{
+                                        fontSize: "23px",
+                                        fontWeight: "700",
+                                        borderBottom: " 2px solid #DFDFDF",
+                                    }}
+                                >
+                                    {data.birthdate}
+                                </TableCell>
+                                <TableCell
+                                    style={{
+                                        fontSize: "23px",
+                                        fontWeight: "700",
+                                        borderBottom: " 2px solid #DFDFDF",
+                                    }}
+                                >
+                                    {data.flightCode}
+                                </TableCell>
+                                <TableCell
+                                    style={{
+                                        fontSize: "23px",
+                                        fontWeight: "700",
+                                        borderBottom: " 2px solid #DFDFDF",
+                                    }}
+                                >
+                                    {data.departure}
+                                </TableCell>
+                                <TableCell
+                                    style={{
+                                        fontSize: "23px",
+                                        fontWeight: "700",
+                                        borderBottom: " 2px solid #DFDFDF",
+                                    }}
+                                >
+                                    {data.contact}
+                                </TableCell>
+                                <TableCell
+                                    style={{
+                                        fontSize: "23px",
+                                        fontWeight: "700",
+                                        borderBottom: " 2px solid #DFDFDF",
+                                    }}
+                                >
+                                    <Button
+                                        variant="contained"
+                                        style={{
+                                            margin: "0px 5px",
+                                            backgroundColor: "white",
+                                            border: "1px solid #B5D3F0",
+                                            width: "20px",
+                                        }}
+                                        component={Link}
+                                        to={`/edit/${data.id}`}
+                                    >
+                                        <img src={Edit} alt="edit" width={20} />
+                                    </Button>
+                                    <Button
+                                        variant="contained"
+                                        color="secondary"
+                                        style={{
+                                            padding: "5px",
+                                            margin: "0px 5px",
+                                            backgroundColor: "#FACDCA",
+                                            border: "1px solid #F30B0B",
+                                            width: "20px",
+                                        }}
+                                        onClick={() => deleteData(data.id)}
+                                    >
+                                        <img src={Delete} alt="delete" width={20} />
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+                <Pagination>
+                    <Button onClick={handlePrevPage} disabled={currentPage === 1}>
+                        이전
+                    </Button>
+                    <span>
+                        {currentPage} / {totalPages}
+                    </span>
+                    <Button onClick={handleNextPage} disabled={currentPage === totalPages}>
+                        다음
+                    </Button>
+                </Pagination>
+            </BodyDiv>
+        </MotherDiv>
     );
 };
 
 export default ListPage;
+
+const MotherDiv = styled.div`
+    background-color: #e3eff9;
+    box-sizing: border-box;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+
+    font-size: 20px;
+    font-weight: 500;
+    padding: 20px;
+`;
+
+const TitleDiv = styled.div`
+    box-sizing: border-box;
+    width: 100%;
+    height: 8%;
+    background-color: #4f95da;
+    border-top-left-radius: 25px;
+    border-top-right-radius: 25px;
+    padding: 10px 30px;
+    font-size: 40px;
+    color: #ffffff;
+    font-weight: 900;
+`;
+
+const BodyDiv = styled.div`
+    box-sizing: border-box;
+    background-color: white;
+    width: 100%;
+    height: 90%;
+    padding: 30px;
+`;
+
+const Pagination = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+    button {
+        margin: 0 10px;
+    }
+`;
+
+const SearchDiv = styled.div`
+    width: 100%;
+    height: 15%;
+    background-color: aliceblue;
+    box-sizing: border-box;
+`;
