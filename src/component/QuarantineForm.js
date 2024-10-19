94258553a2a986f087db17ac06fdc29acf1605e4
오지훈 <162779852+appleitsgood@users.noreply.github.com>
2024-10-20 07:19:32 +0900

    minor fix

--- src/component/QuarantineForm.js
+++ src/component/QuarantineForm.js
@@ -438,7 +438,7 @@ const QuarantineForm = forwardRef(({ mode = 'new', existingData = initialState, 
                               options={countryOptions}
                               value={countryOptions.find((option) => option.value === field.value)}
                               onChange={(selectedOption) => field.onChange(selectedOption ? selectedOption.value : '')}
-                              placeholder="출발국가를 선택하세요"
+                              placeholder="출발국가"
                               isSearchable
                               maxMenuHeight={150}
                               styles={{
